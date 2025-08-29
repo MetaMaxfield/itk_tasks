015ac6d37f6c0ed69b3b6edfd5089860e09bc7f9
MetaMaxfield <metamaxfield@gmail.com>
2025-08-29 17:40:30 +0400

    add task_search_num.py, m2
    Course: 1
    Module: 2
    Topic: 8

---
+++ src/m2/task_search_num.py
@@ -0,0 +1,28 @@
+list_nums = [1, 2, 3, 45, 356, 569, 600, 705, 923]
+
+
+def search(number: id):
+    if not (list_nums[0] <= number <= list_nums[-1]):
+        return False
+
+    left = 0
+    right = len(list_nums)
+    while left <= right:
+        i = (right + left) // 2
+        if list_nums[i] == number:
+            return True
+
+        if list_nums[i] > number:
+            right = i - 1
+        elif list_nums[i] < number:
+            left = i + 1
+
+    return False
+
+
+if __name__ == "__main__":
+    for num in range(-1000, 1001):
+        if num in list_nums:
+            assert search(num)
+        else:
+            assert not search(num)
